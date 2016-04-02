7d9dc5f735d4890b199bc10de86f43fa82234a6e
Jeff Foster <jeff.foster@acm.org>
2016-04-02 18:20:19 +0100

    static routing

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ bot.add('/', function (session) {
 
 // Setup Restify Server
 var server = restify.createServer();
+
+server.get(/.html/, restify.serveStatic({
+    directory: __dirname
+}));
+
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+
 server.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
